5a1f37f072605202b772acab56af5ed86e2a6ed6
LuckyMars <1271975669@qq.com>
2016-07-02 15:14:18 +0800

    删除密码

--- 煎蛋抓取/静态字符串.py
+++ 煎蛋抓取/静态字符串.py
@@ -7,14 +7,6 @@ class Seting():
                'Mozilla/5.0 (Windows NT 10) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11')
     jandanurl = "http://www.jandan.net/"
 
-    # _ConnString = {
-    #     'host': '116.251.219.27',
-    #     'port': '3306',
-    #     'user': 'myadmin',
-    #     'password': 'lipassword',
-    #     'database': 'jandan'}
-
-
     _ConnString = {
         'host': '116.251.219.27',
         'port': '3306',
